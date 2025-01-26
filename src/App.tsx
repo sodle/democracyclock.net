a713d7b5b75580de7ce356d4e0663594d5338d90
Scott Odle <scott@sjodle.com>
2025-01-25 19:37:29 -0700

    countdowns

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,31 @@ function pluralize(value: number, unit: string) {
   return `${value.toLocaleString()} ${unit}s`;
 }
 
-function Countdown(props: { endDate: Date }) {
+function Countdown(props: {
+  endDate: Date;
+  countUp?: boolean;
+  startDate?: Date;
+}) {
   const end = props.endDate.getTime();
+  const start = props.startDate ? props.startDate.getTime() : 0;
+  const duration = end - start;
 
   function calc() {
+    if (props.countUp) {
+      return Math.floor((Date.now() - end) / 1000);
+    }
     return Math.floor((end - Date.now()) / 1000);
   }
 
+  function calcProgress() {
+    if (props.startDate && !props.countUp) {
+      return ((Date.now() - start) / duration) * 100;
+    }
+    return 0;
+  }
+
   const [diff, setDiff] = useState(calc());
+  const [progress, setProgress] = useState(calcProgress());
 
   function breakdown(time: number): {
     days: number;
@@ -40,19 +57,26 @@ function Countdown(props: { endDate: Date }) {
 
   useInterval(() => {
     setDiff(calc());
+    setProgress(calcProgress());
   }, 500);
 
   const b = breakdown(diff);
   return (
     <div>
-      <p>
+      <p className="countdown-relative">
         <strong>{pluralize(b.days, "day")}, </strong>
         {pluralize(b.hours, "hour")}, {pluralize(b.minutes, "minute")},{" "}
         {pluralize(b.seconds, "second")}
       </p>
+      {props.startDate ? (
+        <progress className="countdown-progress" max={100} value={progress} />
+      ) : null}
+      <p className="countdown-end">{props.endDate.toDateString()}</p>
     </div>
   );
 }
+
+const worstDate = new Date("2025-01-20T09:00:00-05:00");
 
 const midtermsDate = new Date("2026-11-03T09:00:00-05:00");
 const electionDate = new Date("2028-11-07T09:00:00-05:00");
@@ -63,20 +87,30 @@ function App() {
     <>
       <h1>Countdown to the end of Trump 2</h1>
       <div>
-        <h2>The 2026 Midterms are in</h2>
-        <Countdown endDate={midtermsDate} />
-        <p>({midtermsDate.toDateString()})</p>
+        <h2>Trump's second term has lasted</h2>
+        <Countdown endDate={worstDate} countUp />
       </div>
+      <hr />
+      <div>
+        <h2>The 2026 Midterms are in</h2>
+        <Countdown endDate={midtermsDate} startDate={worstDate} />
+      </div>
+      <hr />
       <div>
         <h2>The 2028 Presidential Election is in</h2>
-        <Countdown endDate={electionDate} />
-        <p>({electionDate.toDateString()})</p>
+        <Countdown endDate={electionDate} startDate={worstDate} />
       </div>
+      <hr />
       <div>
         <h2>The next President will be inaugurated in</h2>
-        <Countdown endDate={inaugurationDate} />
-        <p>({inaugurationDate.toDateString()})</p>
+        <Countdown endDate={inaugurationDate} startDate={worstDate} />
       </div>
+      <hr />
+      <p>
+        <a href="https://github.com/sodle/democracyclock.net" target="_blank">
+          Source Code
+        </a>
+      </p>
     </>
   );
 }
