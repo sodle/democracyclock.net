91ef41a2dc239d4461d42b83be8463406ca09469
Scott Odle <scott@sjodle.com>
2025-05-03 15:44:37 -0600

    currency quotes

--- worker/index.ts
+++ worker/index.ts
@@ -11,10 +11,18 @@ type Quote = {
   Amount: number;
 };
 
-const lastQuoteDate = "2025-01-17";
+type CurrencyRates = {
+  USD: number;
+};
+
+type CurrencyResponse = {
+  rates: CurrencyRates;
+};
 
 export interface Env {
   FOOL_API_KEY: string;
+  EXRATE_API_KEY: string;
+  DB: D1Database;
 }
 
 export default {
@@ -22,18 +30,78 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/stonks.json") {
+      const startingQuote: ChartBar = {
+        PricingDate: "2025-01-17",
+        Close: { Amount: 5996.66 },
+      };
+      const now = new Date();
+
+      const result = await env.DB.prepare(
+        "SELECT * FROM StonkQuotes ORDER BY timestamp DESC LIMIT 1"
+      ).first();
+
+      if (result !== null) {
+        const timestamp = result["timestamp"] as number;
+        const age = now.getTime() - timestamp;
+
+        if (age < 18 * 60 * 60 * 1000) {
+          const latestQuote: ChartBar = {
+            PricingDate: `${now.getFullYear()}-${
+              now.getMonth() + 1
+            }-${now.getDate()}`,
+            Close: { Amount: result["sp500"] as number },
+          };
+
+          return Response.json({ startingQuote, latestQuote });
+        }
+      }
+
       const response = await fetch(
         `https://api.fool.com/quotes/v4/historical/charts/220472?timeFrame=YTD&precision=Day&apikey=${env.FOOL_API_KEY}`
       );
       const responseJson: FoolResponse = await response.json();
       const chartBars = responseJson.ChartBars;
-
-      const startingQuote = chartBars.find(
-        (b) => b.PricingDate === lastQuoteDate
-      );
       const latestQuote = chartBars[chartBars.length - 1];
 
+      await env.DB.prepare(
+        "INSERT INTO StonkQuotes (timestamp, sp500) VALUE (?, ?)"
+      )
+        .bind(now.getTime(), latestQuote.Close.Amount)
+        .run();
+
       return Response.json({ startingQuote, latestQuote });
+    }
+    if (url.pathname === "/api/dollar.json") {
+      const usdStart = 1 / 1.027707;
+      const now = new Date();
+
+      const result = await env.DB.prepare(
+        "SELECT * FROM CurrencyQuotes ORDER BY timestamp DESC LIMIT 1"
+      ).first();
+
+      if (result !== null) {
+        const timestamp = result["timestamp"] as number;
+        const age = now.getTime() - timestamp;
+
+        if (age < 18 * 60 * 60 * 1000) {
+          const usdLatest = result["usd"] as number;
+          return Response.json({ usdStart, usdLatest });
+        }
+      }
+
+      const response = await fetch(
+        `https://api.exchangeratesapi.io/v1/latest?access_key=${env.EXRATE_API_KEY}&base=EUR&symbols=USD`
+      );
+      const responseJson: CurrencyResponse = await response.json();
+      const usdLatest = 1 / responseJson.rates.USD;
+
+      await env.DB.prepare(
+        "INSERT INTO CurrencyQuotes (timestamp, usd) VALUE (?, ?)"
+      )
+        .bind(now.getTime(), responseJson.rates.USD)
+        .run();
+
+      return Response.json({ usdStart, usdLatest });
     }
     if (url.pathname.startsWith("/api/")) {
       return Response.json({
